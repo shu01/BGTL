c6045056455c702cff7ade87b193f5a3a53a196b
Stephanie Hu <stephaniehu@stephaniesmbp2.risd.edu>
2018-02-21 02:45:08 -0500

    init

---
+++ script.js
@@ -0,0 +1,26 @@
+/// Google analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-22357829-1']);
+_gaq.push(['_trackPageview']);
+(function() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();
+
+/// Start of StatCounter Code for Default Guide -->
+  <script type="text/javascript">
+  var sc_project=9025919; 
+  var sc_invisible=1; 
+  var sc_security="d447726c"; 
+  var scJsHost = (("https:" == document.location.protocol) ?
+  "https://secure." : "http://www.");
+  document.write("<sc"+"ript type='text/javascript' src='" +
+  scJsHost+
+  "statcounter.com/counter/counter.js'></"+"script>");
+  </script>
+  <noscript><div class="statcounter"></div></noscript>
+  <!-- End of StatCounter Code for Default Guide -->
